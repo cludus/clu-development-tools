904ff380c3d05beddf1d24fe1442e0faae88049f
gilberto <gilberto.vento@gmail.com>
2025-08-06 16:14:05 -0400

    fix in k3s

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ def copy_files(source_directory, destination_directory):
     try:
         if os.path.exists(destination_directory) and os.path.exists(destination_directory + '/.bin'):
             try:
-                shutil.rmtree(destination_directory)
-                print(f"Directory '{destination_directory}' removed successfully.")
+                shutil.rmtree(destination_directory + '/.bin')
+                print(f"Directory '{destination_directory}/.bin' removed successfully.")
             except shutil.Error as e:
                 print(f"Error deleting directory: {e}")
             except OSError as e:
                 print(f"Error removing directory '{destination_directory}': {e}")
-        shutil.copytree(source_directory, destination_directory)
+        shutil.copytree(source_directory, destination_directory, dirs_exist_ok=True)
         print(f"Directory '{source_directory}' and its contents copied to '{destination_directory}' successfully.")
     except shutil.Error as e:
         print(f"Error copying directory: {e}")
@@ -77,21 +77,25 @@ def create_gen_root(data):
 def create_ssh_copy_ids(data):
     render_template('ssh-copy-ids.j2', data, '.cluster/.bin/ssh-copy-ids')
 
-# def give_exec_access():
-#     result = subprocess.run(["chmod", "+x", ".cluster/.bin/*"], capture_output=True, text=True, check=True)
-#     print("Stdout:", result.stdout)
-#     print("Stderr:", result.stderr)
+def give_exec_access():
+    give_exec_access_to_file(".cluster/.bin/ssh-copy-ids")
+    give_exec_access_to_file(".cluster/.bin/gen-root-ca")
+
+def give_exec_access_to_file(path):
+    result = subprocess.run(["chmod", "+x", path], capture_output=True, text=True, check=True)
+    # print("Stdout:", result.stdout)
+    # print("Stderr:", result.stderr)
 
 def create_setup(data):
     print(f'creating setup for  {data['name']}')  # Press Ctrl+8 to toggle the breakpoint.
-    # remove_cluster_dir()
+    remove_cluster_dir()
     copy_files('files', '.cluster')
     create_vagrant_file(data)
     create_ansible_inventory(data)
     create_hosts(data)
     create_gen_root(data)
     create_ssh_copy_ids(data)
-    #give_exec_access()
+    give_exec_access()
 
 def read_config(filename):
     try:
